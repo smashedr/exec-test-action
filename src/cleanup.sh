f42d95e9ee546ff865d8b3dc7a7263dc5a5ad6e1
Shane <6071159+smashedr@users.noreply.github.com>
2025-09-27 17:12:22 -0700

    cleaner

--- src/cleanup.sh
+++ src/cleanup.sh
@@ -8,11 +8,10 @@ set -e
 echo "Docker Context - ${GITHUB_ACTION_REF} - Cleanup SSH"
 
 echo "STATE_SSH_CLEANUP: ${STATE_SSH_CLEANUP}"
+#if [[ -n "${STATE_SSH_CLEANUP}" ]];then
 
-if [[ -n "${STATE_SSH_CLEANUP}" ]];then
-    echo "🧹 Cleaning Up authorized_keys"
-    ssh -o StrictHostKeyChecking=no -o BatchMode=yes -o ConnectTimeout=30 -p "${INPUT_PORT}" "${INPUT_USER}@${INPUT_HOST}" \
-        "sed -i '/docker-stack-deploy-action/d' ~/.ssh/authorized_keys"
+echo "Cleaning Up authorized_keys File"
+ssh -o StrictHostKeyChecking=no -o BatchMode=yes -o ConnectTimeout=30 -p "${INPUT_PORT}" "${INPUT_USER}@${INPUT_HOST}" \
+    "sed -i '/docker-stack-deploy-action/d' ~/.ssh/authorized_keys"
 
-    echo -e "🧹 \u001b[32;1m Cleanup SSH Complete"
-fi
+echo -e "🧹 \u001b[32;1m Cleanup SSH Complete"
